0e327b0763e5abf1c4b3cb477412472f3e8fadca
JoseSalamancaCoy <jose.salamancacoy@gmail.com>
2024-10-02 23:20:42 -0500

    Configure Amplify in Ionic

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { Amplify } from 'aws-amplify';
+
+import config from './amplifyconfiguration.json';
+
+Amplify.configure(config);
 
 bootstrapApplication(AppComponent, {
   providers: [
