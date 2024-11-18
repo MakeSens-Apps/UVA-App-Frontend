ca66aba40e4a89a561a3c6532872e770409d15b7
JoseSalamancaCoy <jose.salamancacoy@gmail.com>
2024-11-18 00:06:38 -0500

    Feat: 🆕 Include AutoTraking analitics & identifyUser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { ConfigurationAppService } from './core/services/storage/configuration-app.service';
+import { configureAutoTrack } from 'aws-amplify/analytics';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -15,5 +16,38 @@ export class AppComponent {
    */
   constructor(private configuration: ConfigurationAppService) {
     void this.configuration.loadBranding();
+    this.initAutoTrack();
+  }
+
+  /**
+   * Configure track analitics
+   * @memberof AppComponent
+   */
+  private initAutoTrack(): void {
+    configureAutoTrack({
+      enable: true,
+      type: 'session',
+      options: {
+        attributes: {
+          customizableField: 'attr',
+        },
+      },
+    });
+    configureAutoTrack({
+      enable: true,
+      type: 'pageView',
+      options: {
+        attributes: {
+          customizableField: 'attr',
+        },
+
+        eventName: 'pageView',
+        appType: 'singlePage',
+
+        urlProvider: () => {
+          return window.location.origin + window.location.pathname;
+        },
+      },
+    });
   }
 }
