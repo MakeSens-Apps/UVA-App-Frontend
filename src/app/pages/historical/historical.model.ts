640cd56eb88b70c81db756542751378d2ed46888
JoseSalamancaCoy <jose.salamancacoy@gmail.com>
2024-11-28 17:17:51 -0500

    refactor: 🔧 Names and init of variables in historical.page

---
+++ src/app/pages/historical/historical.model.ts
@@ -0,0 +1,29 @@
+export type TimeFrame = 'month' | 'year';
+
+export type TypeView = 'calendar' | 'chart';
+
+export interface CompleteTaskHistorical {
+  mes: number;
+  date: Date;
+  name: string;
+  daysComplete: number[];
+  daysIncomplete: number[];
+}
+export type MeasurementEntry = Record<string, number>; // Mapea timestamps a valores numéricos
+
+export type HistoricalMeasurement = Record<string, MeasurementEntry[]>;
+
+export const monthsNames = [
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre',
+];
